5bd3f25a76fed648c496b6576e27067517edaa8c
Maria Lymperaiou <maria.lymperaiou@gmail.com>
2017-07-18 16:42:42 +0300

    Add files via upload

---
+++ fft_global.m
@@ -0,0 +1,70 @@
+function [F_Y_mask, F_Cb_mask, F_Cr_mask, RGB] = fft_global(I, r_Y, r_C, fig)
+%στον παρακάτω κωδικα διατηρήθηκαν σε σχόλια οι ενδιάμεσες εικόνες όπως
+%δίνονταν από τον κώδικα των διαφανειών
+
+% 1. FFT on entire image
+
+%if (nargin < 2), r_Y = 0.8; end
+%if (nargin < 3), r_C = 0.3; end
+if (nargin < 4), fig = 1; end
+
+% initialization
+RGB = double(I) / 255;
+
+% scaling for log images
+sc = 0.5;
+
+% RGB->YCBCR
+YCbCr = (rgb2ycbcr(RGB));
+Y = YCbCr(:,:,1);   %εκχωρώ κάθε συνιστώσα σε μια στήλη
+Cb = YCbCr(:,:,2);
+Cr = YCbCr(:,:,3);
+
+% FFT transform
+
+% Y channel
+F_Y = fftshift(fft2(Y));
+if(fig)
+	F_Y_log = sc * log10(1 + abs(F_Y));
+	[fx,fy] = freqspace(size(F_Y));
+	%figure, mesh(fx, fy, F_Y_log), title('3D shifted abs of FFT2 of Y channel');
+	%figure, imshow(F_Y_log / max(max(F_Y_log))), title('shifted abs of FFT2 of Y channel');
+end
+
+% Cb channel
+F_Cb = fftshift(fft2(Cb));
+if(fig)
+	F_Cb_log = sc * log10(1 + abs(F_Cb));
+	[fx,fy] = freqspace(size(F_Cb));
+	%figure, mesh(fx, fy, F_Cb_log), title('3D shifted abs of FFT2 of Cb channel');
+	%figure, imshow(F_Cb_log / max(max(F_Cb_log))), title('shifted abs of FFT2 of Cb channel');
+end
+
+% Cr channel
+F_Cr = fftshift(fft2(Cr));
+if(fig)
+	F_Cr_log = sc * log10(1 + abs(F_Cr));
+	[fx,fy] = freqspace(size(F_Cr));
+	%figure, mesh(fx, fy, F_Cr_log), title('3D shifted abs of FFT2 of Cr channel');
+	%figure, imshow(F_Cr_log / max(max(F_Cr_log))), title('shifted abs of FFT2 of Cr channel');
+end
+
+% mask application
+[r, c] = size(Y);
+[fx, fy] = meshgrid((1:c) - (c+1)/2, (1:r) - (r+1)/2);
+rc = min(r, c) / 2;
+
+% Y channel
+m_Y = zeros(r, c);
+m_Y(sqrt(fx.^2 + fy.^2) < r_Y * rc) = 1;
+F_Y_mask = F_Y .* m_Y;
+%if(fig), figure, mesh(fx, fy, m_Y), title('mask for Y'), end
+
+% Cb and Cr channels
+m_C = zeros(r, c);
+m_C(sqrt(fx.^2 + fy.^2) < r_C * rc) = 1;
+F_Cb_mask = F_Cb .* m_C;
+F_Cr_mask = F_Cr .* m_C;
+%if(fig), figure, mesh(fx, fy, m_C), title('mask for Cb and Cr'), end
+
+
---
+++ ifft_global.m
@@ -0,0 +1,24 @@
+function [iRGB] = ifft_global(F_Y_mask, F_Cb_mask, F_Cr_mask)
+
+%ifft
+
+%Y
+i_F_Y=ifftshift(F_Y_mask);      % ifftshift μετά την εφαρμογή της μάσκας
+i_FY = abs(ifft2(i_F_Y));       % ifft2
+iYCbCr(:,:,1) = i_FY;           % reformation
+
+%Cb
+i_F_Cb=ifftshift(F_Cb_mask);    % ifftshift
+i_FCb = abs(ifft2(i_F_Cb));     % ifft2
+iYCbCr(:,:,2) = i_FCb;          % reformation
+
+%Cr
+i_F_Cr=ifftshift(F_Cr_mask);    % ifftshift
+i_FCr = abs(ifft2(i_F_Cr));     % ifft2
+iYCbCr(:,:,3) = i_FCr;          % reformation
+
+iRGB = ycbcr2rgb(iYCbCr);                           %Μετατροπή χρωματικού χώρου(από YCbCr σε RGB)
+figure,imshow(iRGB),title('global-FFT image');      %Απεικόνιση έγχρωμης ανακατασκευασμένης εικόνας (iRGB)
+
+
+
